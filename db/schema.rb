d638ca5a303af7c43c6c5ff5f2638dd446d3d032
andrewfreda <andrewfreda@gmail.com>
2014-11-09 16:09:22 -0500

    run migration and add shirts association to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107190813) do
+ActiveRecord::Schema.define(version: 20141109181230) do
+
+  create_table "shirts", force: true do |t|
+    t.string   "name"
+    t.float    "length"
+    t.float    "sleeve"
+    t.float    "chest"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
