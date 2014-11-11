5496e0760c008c87eafb05185ca1570398a08543
andrewfreda <andrewfreda@gmail.com>
2014-11-11 16:36:09 -0500

    Never redirect to user page anymore, making shirts index page as new lobby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      redirect_to user_path(id: @user.id)
+      redirect_to shirts_path
     else
       flash[:alert] = 'Password and confirmation do not match'
       render new_user_path
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.find_by(email: user_params[:email])
     if @user and @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
-      redirect_to(user_path(id: @user.id), notice: "Welcome back, #{@user.email}")
+      redirect_to(shirts_path, notice: "Welcome back, #{@user.email}")
     else
       @user = User.new
       flash[:alert] = 'Invalid email or password'
@@ -39,12 +39,12 @@ class UsersController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to new_user_path
+    redirect_to login_user_path
   end
 
   def check_auth
     if current_user
-      redirect_to(user_path(id: session[:user_id]))
+      redirect_to shirts_path
     else
       @user = User.new
     end
