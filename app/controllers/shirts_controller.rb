5c5008f367e4791920117647904105d73215fbfc
andrewfreda <andrewfreda@gmail.com>
2014-11-22 14:42:00 -0500

    Add function to draw a Shirt on a given canvas

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -59,6 +59,31 @@ class ShirtsController < ApplicationController
   end
 
 
+  def draw_shirt(canvas, shirt, fill_color = ChunkyPNG::Color::TRANSPARENT)
+    length_mod = shirt.length * 5
+    chest_mod  = shirt.chest  * 5
+    sleeve_mod = shirt.sleeve * 5
+
+    x = 50
+    y = 50
+
+    first_pt   = ChunkyPNG.Point(x, y)
+    second_pt  = ChunkyPNG.Point(x + chest_mod,                       y)
+    third_pt   = ChunkyPNG.Point(x + chest_mod + (0.75 * sleeve_mod), y + (0.10 * length_mod))
+    fourth_pt  = ChunkyPNG.Point(x + chest_mod + (0.75 * sleeve_mod), y + (0.33 * length_mod))
+    fifth_pt   = ChunkyPNG.Point(x + chest_mod,                       y + (0.33 * length_mod))
+    sixth_pt   = ChunkyPNG.Point(x + chest_mod,                       y + length_mod)
+    seventh_pt = ChunkyPNG.Point(x,                                   y + length_mod)
+    eighth_pt  = ChunkyPNG.Point(x,                                   y + (0.33 *length_mod))
+    ninth_pt   = ChunkyPNG.Point(x - (0.75 * sleeve_mod),             y + (0.33 * length_mod))
+    tenth_pt   = ChunkyPNG.Point(x - (0.75 * sleeve_mod),             y + (0.10 * length_mod))
+
+    outline = ChunkyPNG.Vector(first_pt, second_pt, third_pt, fourth_pt, fifth_pt, 
+                                sixth_pt, seventh_pt, eighth_pt, ninth_pt, tenth_pt)
+
+    canvas.polygon(outline, ChunkyPNG::Color::BLACK, fill_color)
+  end
+
   private
     def shirt_params
       params.require(:shirt).permit(:name, :length, :chest, :sleeve, :user_id)
